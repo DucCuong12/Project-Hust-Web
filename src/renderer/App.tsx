54143d135a822d4f207f20d4749226a1e394bbf4
hongdangn <ddangdangnh@gmail.com>
2024-09-29 09:54:38 +0700

    add NavBar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,28 +1,19 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from '../components/LoginForm/LoginForm';
-import './App.css';
+// import './App.css';
 import { useLocation } from 'react-router-dom';
-import AnimatedFrame from '../../utils/animation_page';
 import SignupForm from '../components/SignupForm/SignupForm';
 import AccountManage from '../components/AccountManage/AccountManage';
+import NavBar from '../components/HomePage/NavBar';
 
-function Hello() {
-  return (
-    <AnimatedFrame>
-      <div>
-        <h2>Hello World!</h2>
-      </div>
-    </AnimatedFrame>
-  );
-}
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/home" element={<Hello />} />
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/manage-account" element={<AccountManage />} />
+        {/* <Route path="/signup" element={<SignupForm />} /> */}
+        <Route path="/" element={<NavBar />} />
+        {/* <Route path="/" element={<LoginForm />} /> */}
+        {/* <Route path="/manage-account" element={<AccountManage />} /> */}
       </Routes>
     </Router>
   );
