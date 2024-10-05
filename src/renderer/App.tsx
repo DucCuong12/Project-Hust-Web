579f773e33dd6f196d49e75ec074991c4619d9af
hongdangn <ddangdangnh@gmail.com>
2024-10-05 21:20:58 +0700

    fix UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from '../components/LoginForm/LoginForm';
-import './App.css';
+// import './App.css';
 import SignupForm from '../components/SignupForm/SignupForm';
 import AccountManage from '../components/AccountManage/AccountManage';
 import HomePage from '../components/Pages/HomePage/HomePage';
@@ -9,10 +9,10 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignupForm />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/manage-account" element={<AccountManage />} />
+        {/* <Route path="/signup" element={<SignupForm />} /> */}
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<LoginForm />} /> */}
+        {/* <Route path="/manage-account" element={<AccountManage />} /> */}
       </Routes>
     </Router>
   );
